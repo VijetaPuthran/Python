a1559134dad0bf826977ee0523c622a47df3baa3
VP <vijeta.puthran@gmail.com>
2021-01-15 00:11:59 -0600

    Updated code

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,8 +14,21 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     #print(f"CSV Header: {csv_header}")
     months = []
+    netTotal = []
+    sum = 0
     # Read each row of data after the header
     for row in csvreader:
         months.append(row)
-        
-    print("The total number of months included in the dataset is " + str(len(months)) + " months.")
+        print("The total number of months included in the dataset is " + str(len(months)) + " months.")
+
+    for row1 in csvreader:
+        netTotal.append(row1(1)
+            for x in netTotal
+                sum = sum + x
+            print(sum)    
+
+  #  sum = 0
+#for x in [1,2,3,4,5]:
+ #     sum = sum + x
+
+#print(sum)
