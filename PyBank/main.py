2be586a6cd84941a286c3c2668117a5e15d0eae7
VP <vijeta.puthran@gmail.com>
2021-01-17 10:11:32 -0600

    Updated PyBank script

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,32 +3,26 @@ import csv
 
 csvpath = os.path.join('Resources', '02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')
 
-with open(csvpath) as csvfile:
+with open(csvpath,encoding="utf-8") as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    #print(csvreader)
-
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
+  
     months = []
-    netTotal = []
     sum = 0
+    average_change = []
+
     # Read each row of data after the header
     for row in csvreader:
         months.append(row)
-        print("The total number of months included in the dataset is " + str(len(months)) + " months.")
+        sum +=int(row[1])
+        average_change.append((row+1) - row)
 
-    for row1 in csvreader:
-        netTotal.append(row1(1)
-            for x in netTotal
-                sum = sum + x
-            print(sum)    
 
-  #  sum = 0
-#for x in [1,2,3,4,5]:
- #     sum = sum + x
+    print("Total months: " + str(len(months)))
+    print("Total: " + "$"+ str(sum))
+    print(average_change)
 
-#print(sum)
