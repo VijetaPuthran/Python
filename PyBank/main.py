d9e3e974f816bc13d649311aaa15bc0defe9c38a
VP <vijeta.puthran@gmail.com>
2021-01-17 11:20:11 -0600

    Updated calculations in code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,6 @@
 import os
 import csv
+from statistics import mean
 
 csvpath = os.path.join('Resources', '02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')
 
@@ -10,19 +11,43 @@ with open(csvpath,encoding="utf-8") as csvfile:
 
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
-  
+    
+    #Initializing the variables
     months = []
     sum = 0
-    average_change = []
+    profit_losses = []
+    profit_loss_change = []
+    average = 0
 
     # Read each row of data after the header
     for row in csvreader:
-        months.append(row)
+    #Adding the elements to the months[], using the first column of the csv file
+        months.append(row[0])
+    #Net total of Profit/Losses over the entire period, using the second column of the csv file     
         sum +=int(row[1])
-        average_change.append((row+1) - row)
+    #Adding the elements to the profit_losses[], using the second column of the csv file 
+        profit_losses.append(int(row[1]))
+    #For loop to iterate through the profit_losses[] to calculate the change in profit/loss        
+    for row in range(len(profit_losses)-1):
+        profit_loss_change.append(profit_losses[row+1]-profit_losses[row])
+    #Python in-built function to calculate mean
+    def Average(profit_loss_change):
+        return mean(profit_loss_change)
 
+    average = Average(profit_loss_change)
+    #Calculation to get the index of the month from the month[], for max and min changes in profit/loss respectively
+    max_month = profit_loss_change.index(max(profit_loss_change)) + 1
+    min_month = profit_loss_change.index(min(profit_loss_change)) + 1
+    max_month_profit_loss_change = months[max_month]
+    min_month_profit_loss_change = months[min_month]
 
+    print("Financial Analysis")
+    print("----------------------------")
     print("Total months: " + str(len(months)))
     print("Total: " + "$"+ str(sum))
-    print(average_change)
+    print("Average Change: " + "$"+ str(round(average, 2)))
+    print("Greatest Increase in Profits: " + str(max_month_profit_loss_change) + " " +"($" + str(max(profit_loss_change)) +")")
+    print("Greatest Decrease in Profits: " + str(min_month_profit_loss_change) + " " +"($" + str(min(profit_loss_change)) +")")
+
+
 
